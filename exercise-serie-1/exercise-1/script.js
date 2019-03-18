137bfcde9c014a6526cc0b4b88a01270299c9495
Christophe Schevers <christophe.schevers@gmail.com>
2019-03-18 13:38:33 +0100

    New commit

--- exercise-serie-1/exercise-1/script.js
+++ exercise-serie-1/exercise-1/script.js
@@ -3,3 +3,14 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+function colorChangeBlue() {
+    document.getElementById("square").style.backgroundColor = "blue";
+}
+
+function colorChangeRed() {
+    document.getElementById("square").style.backgroundColor = "red";
+}
+
+function colorChangeGreen() {
+    document.getElementById("square").style.backgroundColor = "green";
+}
