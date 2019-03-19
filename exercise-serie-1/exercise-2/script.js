be79523e00553c3abca98c06dfc229077f0ec484
Christophe Schevers <christophe.schevers@gmail.com>
2019-03-19 09:23:10 +0100

    New commit

--- exercise-serie-1/exercise-2/script.js
+++ exercise-serie-1/exercise-2/script.js
@@ -1,5 +1,31 @@
 //Check to see if script is linked properly.
 console.log("This script is linked properly!")
 
-
 //Write your JS code here...
+function addBtn() {
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    result = n1 + n2;
+    document.getElementById("result").innerHTML = `The result is ${result}.`;
+}
+
+function subBtn() {
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    result = n1 - n2;
+    document.getElementById("result").innerHTML = `The result is ${result}.`;
+}
+
+function multiBtn() {
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    result = n1 * n2;
+    document.getElementById("result").innerHTML = `The result is ${result}.`;
+}
+
+function divideBtn() {
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    result = n1 / n2;
+    document.getElementById("result").innerHTML = `The result is ${result}.`;
+}
