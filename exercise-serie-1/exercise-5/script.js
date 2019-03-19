a68a1e09f6849cf89312c3feac932861305392fe
Christophe Schevers <christophe.schevers@gmail.com>
2019-03-19 16:48:56 +0100

    New commit

--- exercise-serie-1/exercise-5/script.js
+++ exercise-serie-1/exercise-5/script.js
@@ -3,3 +3,17 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+let slideIndex = 0;
+catCycler();
+
+function catCycler() {
+    let i;
+    let x = document.getElementsByClassName("gallery-img");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(catCycler, 1000);
+}
