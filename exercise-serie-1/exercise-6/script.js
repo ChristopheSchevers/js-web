756479dcc501c7bc90db17b5542c6d4bbb8b23fd
Christophe Schevers <christophe.schevers@gmail.com>
2019-03-20 11:56:23 +0100

    New commit

--- exercise-serie-1/exercise-6/script.js
+++ exercise-serie-1/exercise-6/script.js
@@ -3,3 +3,10 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+let pics = document.getElementById("picstore");
+
+function randomCats() {
+    for (i = pics.children.length; i >= 0; i--) {
+        pics.appendChild(pics.children[Math.random() * i | 0]);
+    }
+}
