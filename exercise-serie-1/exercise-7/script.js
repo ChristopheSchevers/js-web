1f1a2e609d18dcf581ee0ee84f7a9f76b59088d2
Christophe Schevers <christophe.schevers@gmail.com>
2019-03-20 16:32:24 +0100

    New commit

--- exercise-serie-1/exercise-7/script.js
+++ exercise-serie-1/exercise-7/script.js
@@ -3,3 +3,40 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+let counter = 0;
+
+document.addEventListener("click", function() {
+    counter += 1;
+    switch (counter) {
+        case 7:
+            alert("What's the matter, buddy? Clicking a button too hard for you?");
+            break;
+        case 15:
+            alert("Come on, slowpoke! What are you waiting for? Click the button!");
+            break;
+        case 27:
+            alert("Oh my God! Seriously?");
+            break;
+        case 35:
+            alert("NOOOOOOOOOOOOOOB!!!");
+            break;
+    }
+});
+
+function initTroll() {
+
+    let btn = document.getElementById("troll");
+
+    let btn_V = btn.clientHeight;
+    let btn_H = btn.clientWidth;
+
+    let availSpace_V = window.innerHeight - btn_V;
+    let availSpace_H = window.innerWidth - btn_H;
+
+    btn.style.top = Math.round(Math.random() * availSpace_V) + "px";
+    btn.style.left = Math.round(Math.random() * availSpace_H) + "px";    
+}
+
+/*function clickR() {
+    counter += 1;
+}*/
