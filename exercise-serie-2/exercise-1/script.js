3aa2c7e50b3ab40a4c68f31553b6b35be582d877
Christophe Schevers <christophe.schevers@gmail.com>
2019-03-21 14:59:53 +0100

    New commit

--- exercise-serie-2/exercise-1/script.js
+++ exercise-serie-2/exercise-1/script.js
@@ -3,3 +3,23 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+// This code was inspired by NotJoey
+let div = document.getElementById("img-container");
+
+function catCyclerTwo() {
+    axios.get('https://aws.random.cat/meow')
+        .then(function (response) {
+            div.style.backgroundImage = "url("+response['data']['file']+")";
+            console.log(response['data']['file']);
+            div.style.backgroundRepeat = "no-repeat";
+            div.style.backgroundPosition = "center";
+            div.style.backgroundSize = "cover";
+            div.style.minHeight = "500px";
+            div.style.maxHeight = "500px";
+            div.style.minWidth = "500px";
+            div.style.maxWidth = "500px";
+        })
+    setTimeout(function(){
+        catCyclerTwo();
+    }, 3000);
+}
